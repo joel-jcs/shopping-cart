56374e8a61bef8a9fef1b14de2f1a7effd9defe5
joel-jcs <joeljcs@protonmail.com>
2024-10-25 18:44:08 -0400

    updated promo banners, and the styles for banners and categories

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -1,6 +1,8 @@
-export default function PromoBanner({ imageSrc }) {
+export default function PromoBanner({ title, subtitle, imageSrc }) {
   return (
     <div className="promoContainer">
+      <h1 className="promoTitle">{title}</h1>
+      <h2 className="promoSubtitle">{subtitle}</h2>
       <img src={imageSrc} className="promoBanner" />
       <button className="promoButton">SHOP NOW</button>
     </div>
