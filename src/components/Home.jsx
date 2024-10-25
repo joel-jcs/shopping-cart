79cf750575c78d07e8d356dded9c54a337084412
joel-jcs <joeljcs@protonmail.com>
2024-10-24 22:37:31 -0400

    initial structure and stying for home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+export default function Home() {
+  return (
+    <div className="homeContainer">
+      <h1>Home</h1>
+      <div className="promoContainer">
+        <img
+          src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+          className="promoBanner"
+        />
+        <button className="promoButton">SHOP NOW</button>
+      </div>
+      <div className="categoriesContainer">
+        <div className="categoryCardContainer">
+          <img
+            src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+            alt=""
+          />
+          <button>SHOP</button>
+        </div>
+        <div className="categoryCardContainer">
+          <img
+            src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+            alt=""
+          />
+          <button>SHOP</button>
+        </div>
+        <div className="categoryCardContainer">
+          <img
+            src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+            alt=""
+          />
+          <button>SHOP</button>
+        </div>
+        <div className="categoryCardContainer">
+          <img
+            src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+            alt=""
+          />
+          <button>SHOP</button>
+        </div>
+      </div>
+    </div>
+  );
+}
