86cb6d62c37d548cd6f8c003d3f1cdda45a2bc11
joel-jcs <joeljcs@protonmail.com>
2024-10-26 14:40:55 -0400

    created product card component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,34 @@
+export default function ProductCard({ product }) {
+  return (
+    <div
+      className="productCard flex flex-col items-center justify-center gap-3 rounded-lg px-1 py-3"
+      key={product.id}
+    >
+      <img
+        src={product.image}
+        className="productImg h-full select-none bg-white object-cover"
+      />
+      <div className="productDetails flex h-auto w-full flex-col items-center py-2">
+        <h2 className="productPrice text-xl font-bold text-red-500">
+          {product.price}
+        </h2>
+        <span className="productName mb-4 text-sm font-bold">
+          {product.name}
+        </span>
+        <div className="productQtyContainer flex h-1/4 w-full items-center justify-center gap-2 py-0">
+          <button>-</button>
+          <input
+            type="number"
+            min="0"
+            max="99"
+            placeholder="1"
+            onInput={(e) => (e.target.value = e.target.value.slice(0, 2))}
+            className="px-2 py-1.5 text-center outline-2 outline-gray-600"
+          />
+          <button>+</button>
+        </div>
+        <button className="mt-3">Add to Cart</button>
+      </div>
+    </div>
+  );
+}
--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import data from "../assets/dummyProductData";
+import ProductCard from "./ProductCard";
 
 export default function Store() {
   {
@@ -22,40 +23,7 @@ export default function Store() {
       <h1 className="text-6xl font-black">CATEGORY NAME</h1>
       <div className="productContainer my-10 flex flex-wrap gap-5 px-10 py-20">
         {products.map((product) => {
-          return (
-            <div
-              className="productCard flex flex-col items-center justify-center gap-3 rounded-lg px-1 py-3"
-              key={product.id}
-            >
-              <img
-                src={product.image}
-                className="productImg h-full select-none bg-white object-cover"
-              />
-              <div className="productDetails flex h-auto w-full flex-col items-center py-2">
-                <h2 className="productPrice text-xl font-bold text-red-500">
-                  {product.price}
-                </h2>
-                <span className="productName mb-4 text-sm font-bold">
-                  {product.name}
-                </span>
-                <div className="productQtyContainer flex h-1/4 w-full items-center justify-center gap-2 py-0">
-                  <button>-</button>
-                  <input
-                    type="number"
-                    min="0"
-                    max="99"
-                    placeholder="1"
-                    onInput={(e) =>
-                      (e.target.value = e.target.value.slice(0, 2))
-                    }
-                    className="px-2 py-1.5 text-center outline-2 outline-gray-600"
-                  />
-                  <button>+</button>
-                </div>
-                <button className="mt-3">Add to Cart</button>
-              </div>
-            </div>
-          );
+          return <ProductCard key={product.id} product={product} />;
         })}
       </div>
     </div>
