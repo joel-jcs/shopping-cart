092508d35249976add54cab7e7c9cd861c817d33
joel-jcs <joeljcs@protonmail.com>
2024-10-26 00:50:30 -0400

    added navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Home from "./components/Home";
+import Navbar from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
