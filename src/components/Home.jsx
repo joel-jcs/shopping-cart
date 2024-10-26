cc8f3e5f1baa977b0c06292e713488a67e50729e
joel-jcs <joeljcs@protonmail.com>
2024-10-26 14:26:06 -0400

    updated promo banner and title styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,15 +7,15 @@ export default function Home() {
   const [categories] = useState(data);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-10">
+    <div className="homeContainer flex flex-col items-center justify-center gap-10">
       <PromoBanner
         title="LIMITED TIME OFFER"
         subtitle="USE 'LETSGOO' AT CHECKOUT FOR 50% OFF"
-        imageSrc="https://c.stocksy.com/a/Iu2P00/z9/5969378.jpg"
+        imageSrc="https://c.stocksy.com/a/cRhF00/z9/3741924.jpg"
       />
-      <div className="mb-10 mt-8 flex flex-col">
-        <h1 className="mb-6 text-4xl font-black">SHOP BY CATEGORY</h1>
-        <div className="flex h-[70vh] w-full items-center justify-center gap-10">
+      <div className="categoryContainer mb-10 mt-8 flex flex-col">
+        <h1 className="mb-10 text-4xl font-black">SHOP BY CATEGORY</h1>
+        <div className="flex h-[70vh] w-screen items-center justify-center gap-10 px-20">
           {categories.map((category) => {
             return (
               <CategoryCard
