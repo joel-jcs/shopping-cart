ef88898b8ebb8da3c4e4eab247fcc19302bf05b7
joel-jcs <joeljcs@protonmail.com>
2024-10-26 02:25:51 -0400

    refactored to tw

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,7 +1,11 @@
 export default function CategoryCard({ imageSrc, altText, categoryName }) {
   return (
-    <div className="relative flex h-full w-1/4 cursor-pointer items-center justify-center">
-      <img src={imageSrc} alt="" className="h-full w-full object-cover" />
+    <div className="relative flex h-full w-1/4 cursor-pointer items-center justify-center rounded-2xl">
+      <img
+        src={imageSrc}
+        alt=""
+        className="h-full w-full rounded-2xl object-cover"
+      />
       <button className="absolute bottom-[10%]">
         SHOP {categoryName.toUpperCase()}
       </button>
