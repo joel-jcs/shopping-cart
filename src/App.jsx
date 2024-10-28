3b6c1a5467c1384da0572c1cd68570bbf15ca530
joel-jcs <joeljcs@protonmail.com>
2024-10-28 18:01:38 -0400

    lifted cartQty states and passed as context

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import "./App.css";
 import Navbar from "./components/Navigation";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
+
+  const [cartQty, setCartQty] = useState(0);
+  useEffect(() => {
+    setCartQty(cart.map((item) => item.qty).reduce((a, b) => a + b, 0));
+  }, [cart]);
+
   return (
     <>
-      <Navbar cart={cart} />
-      <Outlet context={{ cart, setCart }} />
+      <Navbar cartQty={cartQty} />
+      <Outlet context={{ cart, setCart, cartQty }} />
       <Footer />
     </>
   );
